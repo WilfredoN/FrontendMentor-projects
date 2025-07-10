24c17d70a11766eac381c56da430e5068e1e7db8
WilfredoN <thekillermaps@gmail.com>
2025-07-10 15:19:02 +0300

    feat: finishing the accordion, remove non ready project

--- faq-accordion-card-main/scripts/main.js
+++ faq-accordion-card-main/scripts/main.js
@@ -1,8 +1,14 @@
 let expEl = document.querySelectorAll('.title');
 expEl.forEach(li => {
-    li.addEventListener('click', function() {
-        this.classList.toggle('open')
-    },
-    false
-    )
+    li.addEventListener('click', function () {
+        const paragraph = this.nextElementSibling;
+
+        document.querySelectorAll('#paragraph.open').forEach(openParagraph => {
+            if (openParagraph !== paragraph) {
+                openParagraph.classList.remove('open');
+            }
+        });
+
+        paragraph.classList.toggle('open');
+    }, false);
 });
